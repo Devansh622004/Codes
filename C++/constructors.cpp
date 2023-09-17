51f40cee393dd24b74ca4ecd26cd38649c6c070a
Devansh <devanshvashisth5@gmail.com>
2023-09-17 11:59:39 +0530

    commit

--- C++/constructors.cpp
+++ C++/constructors.cpp
@@ -1,3 +1,16 @@
+/*Constructors in C++
+
+A constructor is a special member function with the same name as the class. The constructor doesn’t have a return type. Constructors are used to initialize the objects of its class. Constructors are automatically invoked whenever an object is created.
+
+Important Characteristics of Constructors in C++ :-
+
+1.)A constructor should be declared in the public section of the class
+2.)They are automatically invoked whenever the object is created
+3.)They cannot return values and do not have return types
+4.)It can have default arguments
+5.)We cannot refer to their address
+*/
+
 # include<iostream>
 using namespace std;
 
@@ -23,10 +36,6 @@ void employee :: display()
 int main()
 {
     employee ramprakaas;
-
     ramprakaas.display();
-
-
-
 return 0;
 }
