7759ca3f7f063d8d00df81c36d8c542e3efdfd40
Devansh <devanshvashisth5@gmail.com>
2023-09-17 11:11:06 +0530

    commit

---
+++ C++/pasobjasfuncarg.cpp
@@ -0,0 +1,47 @@
+# include<iostream>
+using namespace std;
+
+class complex
+{
+    int a, b;
+    public:
+        void display();
+        void sum(complex o1, complex o2);
+        complex();                            // CONSTRUCTOR DECLARATION
+};
+
+complex :: complex()                          // CONSTRUCTOR DEFINATION
+{
+    a = 2;
+    b = 4;
+}
+
+void complex :: display()
+{
+    cout<<"THE COMPLEX NO. IS : "<<a<<"+"<<b<<"i";
+}
+
+void complex :: sum(complex o1, complex o2)   // PASSING OBJECTS AS FUNCTION ARGUMENTS
+{
+    a = o1.a + o2.a;
+    b = o1.b + o2.b;
+}
+
+int main()
+{
+    complex c1, c2, c3;
+
+    c1.display();
+
+    cout<<endl;
+
+
+    c2.display();
+    
+    cout<<endl;
+    
+    c3.sum(c1,c2);                      
+    c3.display();
+
+    return 0;
+}
