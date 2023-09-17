4148c7c0492e2d438f81565aac294dea70d2726c
Devansh <devanshvashisth5@gmail.com>
2023-09-17 22:24:55 +0530

    commit

--- C++/multipleconst.cpp
+++ C++/multipleconst.cpp
@@ -1,36 +1,52 @@
-# include<iostream>
+/*Constructor overloading is a concept in which one class can have
+multiple constructors with different parameters. The main thing to note
+here is that the constructors will run according to the arguments for
+example if a program consists of 3 constructors with 0, 1, and 2 arguments
+, so if we pass 1 argument to the constructor the compiler will
+automatically run the constructor which is taking 1 argument.
+ */
+
+#include <iostream>
 using namespace std;
 
-class complex
+class Complex
 {
     int a, b;
-    public:
-        void get(int x, int y);
-        void display();
-        void sum();
+
+public:
+    Complex()
+    {
+        a = 0;
+        b = 0;
+    }
+
+    Complex(int x, int y)
+    {
+        a = x;
+        b = y;
+    }
+
+    Complex(int x)
+    {
+        a = x;
+        b = 0;
+    }
+
+    void printNumber()
+    {
+        cout << "Your number is " << a << " + " << b << "i" << endl;
+    }
 };
-
-void complex :: get(int x, int y)
-{
-    a = x;
-    b = y;
-}
-
-void complex :: display()
-{
-    cout<<"THE COMPLEX NO. IS : "<<a<<"+"<<b<<"i";
-}
 
 int main()
 {
-    complex c1, c2;
-    c1.get(1,2);
-    c1.display();
+    Complex c1(4, 6);
+    c1.printNumber();
 
-    cout<<endl;
+    Complex c2(5);
+    c2.printNumber();
 
-    c2.get(1,2);
-    c2.display();
-    
+    Complex c3;
+    c3.printNumber();
     return 0;
 }
