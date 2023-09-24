a72a5348e6cea5bc0221b63c5bc1cc2293262f6f
Devansh <devanshvashisth5@gmail.com>
2023-09-24 16:54:31 +0530

    commit

---
+++ C/linseach.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    int x, t = 0;
+    printf("ENTER A ONE DIGIT NO., YOU WANTS TO SEARCH\n");
+    scanf("%d", &x);
+
+    for (int i = 0; a[i] != '\0'; i++)
+    {
+        if (a[i] == x)
+        {
+            t++;
+            printf("%d is existed %d times in the array\n", x, t);
+        }
+    }
+}
