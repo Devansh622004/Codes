10ca95f415b0aaa5f1d8761df645f4f9cc5eb171
Devansh <devanshvashisth5@gmail.com>
2023-10-03 13:08:19 +0530

    commit

---
+++ C++/destructor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int count = 0;
+
+class num
+{
+    int count;
+    public:
+    num()
+    {
+        count++;
+        cout << "THIS IS THE TIME WHEN CONSTRUCTOR IS CALLED FOR OBJECT NUMBER" 
+        << count << endl;
+    }
+
+    ~num()
+    {
+        cout<<"THIS IS THE TIME WHEN DESTRUCTOR IS CALLED FOR OBJECT NUMBER"
+        <<count<<endl;
+
+        count--;
+    }
+};
+
+int main()
+{
+    cout<<"WE ARE INSIDE OUR MAIN FUNCTION"<<endl;
+    cout<<"CREATING FIRST n1"<<endl;
+    num n1;
+    {
+        cout<<"ENTERING THIS BLOCK"<<endl;
+        cout<<"CREATING TWO MORE OBJECTS"<<endl;
+        num n2,n3;
+        cout<<"EXITING THIS BLOCK";
+    }
+    cout<<"BACK TO MAIN"<<endl;
+    return 0;
+}
