96cb29c36f75f31326dc27fe0befb95d9f2dd0bb
Devansh <devanshvashisth5@gmail.com>
2023-10-06 19:59:48 +0530

    commit

--- C/binsrch2.c
+++ C/binsrch2.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 void main()
 {
-    int ar[10], x;
+    int ar[10], x, n, beg, mid, end;
+
+    printf("ENTER SIZE OF ARRAY:\n");
+    scanf("%d", &n);
+
     printf("ENTER THE ELEMENTS:\n");
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < n; i++)
     {
         scanf("%d", &ar[i]);
     }
@@ -11,5 +15,28 @@ void main()
     printf("ENTER THE ELEMENT TO BE SEARCH\n");
     scanf("%d", &x);
 
-    
+    beg = 0;
+    end = n - 1;
+    mid = (beg + end) / 2;
+
+    while ((beg <= end) && (x != ar[mid]))
+    {
+        if (x < ar[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            beg = mid + 1;
+            mid = (beg + end) / 2;
+        }
+    }
+    if (ar[mid] == x)
+    {
+        printf("ELEMENT FOUND AT %d", mid + 1);
+    }
+    else
+    {
+        printf("ELEMENT NOT FOUND\n");
+    }
 }
--- C/contacts.c
+++ C/contacts.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef struct contacts
+struct contacts
 {
     char name[20];
     char phno[20];
-}person;
+};
 
 void main()
 {
-    person people[2];
-    char n[20] = "meher";
+    struct contacts people[2];
+    char n[20];
 
     people[0].name[20] = "meher";
     people[0].phno[20] = "+91-8449914409";
 
     people[1].name[20] = "sai";
     people[1].phno[20] = "+91-8923221777";
+    
+    printf("SEARCH BOX: ");
+    gets(n);
 
-
+//	int temp = strcmp(people[i].name, n);
     for (int i = 0; i < 2; i++)
     {
+    	int temp = strcmp(people[i].name, n);
+    	printf("%d", temp);
         if (strcmp(people[i].name, n) == 0)
         {
             printf("Phone Number: %s", people[i].phno);
         }
-        //else
-//        {
-//        	printf("THIS CONTACT IS NOT EXIST\n");
-//        	break;
-//		}
-}
+	}
 	printf("THIS CONTACT IS NOT EXIST\n");
     return 1;
 }
