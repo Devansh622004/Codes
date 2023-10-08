1a213fcee5e752126990edb2aeff7b9ebb7b7dd5
Devansh <devanshvashisth5@gmail.com>
2023-10-08 11:22:57 +0530

    commit

---
+++ C++/new3.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+using namespace std;
+int main()
+{
+    while(true)
+    {
+        int *p = new int;
+    }
+
+    while(true)
+    {
+        int a = 5;
+    }
+
+    delete p; 
+return 0;
+}
