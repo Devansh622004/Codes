1a06d6946f2f217ff92fbef1e7c6d061e827b037
Devansh <devanshvashisth5@gmail.com>
2023-10-08 10:58:14 +0530

    commit

---
+++ C++/new2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "ENTER REQUIRED NO. OF ELEMENTS\n";
+    cin >> n;
+
+    int *p = new int[n];
+
+    return 0;
+}
---
+++ C++/tafree.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+# include<string.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    char arr[n];
+    cout<<strlen(arr);
+
+return 0;
+}
