a77ec830b63c1cafc7cf38908bbdde85ce4ab66d
Devansh <devanshvashisth5@gmail.com>
2025-04-26 17:41:00 +0530

    commit

---
+++ java/Single_inheritance.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+class student1{
+    int rollno;
+    String name;
+
+    Scanner s = new Scanner(System.in);
+    public void getdata(){
+        System.out.println("ENTER ROLL NO.: ");
+        rollno = s.nextInt();
+        System.out.println("ENTER NAME: ");
+        name = s.next();
+    }
+    public void display(){
+        System.out.println("ROLL NO. = " + rollno);
+        System.out.println("NAME = " + name);
+    }
+}
+
+class test1 extends student1{
+    int sub1, sub2;
+    void get(){
+        getdata();
+        System.out.println("ENTER MARKS1: ");
+        sub1 = s.nextInt();
+        System.out.println("ENTER MARKS2: ");
+        sub2 = s.nextInt();
+    }
+    void putdata(){
+        display();
+        System.out.println("MARKS1 = " + sub1);
+        System.out.println("MARKS2 = " + sub2);
+    }
+}
+
+public class Single_inheritence {
+    public static void main(String[] args){
+        test1 obj = new test1();
+        obj.get();
+        obj.putdata();
+    }
+}
