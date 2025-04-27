29c98de7252a89b5e6654fff20f4789fac069351
Devansh <devanshvashisth5@gmail.com>
2025-04-27 11:12:37 +0530

    commit

---
+++ java/heirarchical_inheritance.java
@@ -0,0 +1,4 @@
+package PACKAGE_NAME;
+
+public class heirarchical_inheritance {
+}
--- java/inheritence.java
+++ java/inheritence.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+class base{
+    int a, b;
+    void get(int m, int n){
+        a = m;
+        b = n;
+    }
+    void sum(){
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("THE SUM = " + (a+b));
+    }
+}
+
+class derived extends base{
+    int c;
+    void get2(int o){
+        c = o;
+    }
+    void sum2(){
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("THE SUM = " + (a+b+c));
+    }
+}
 
 public class inheritence {
+    public static void main(String[] args){
+        derived obj = new derived();
+        // ACCESSING BASE CLASS METHODS.
+        obj.get(2, 3);
+        obj.sum();
+
+        System.out.println("**************************************************************************************");
+
+        // ACCESSING DERIVED CLASS METHODS.
+        obj.get2(5);
+        obj.sum2();
+    }
 }
