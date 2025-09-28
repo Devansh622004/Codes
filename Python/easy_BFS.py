73908b039eac90cdd5894ce88757110f4b246d03
Devansh <devanshvashisth5@gmail.com>
2025-09-28 22:09:40 +0530

    commit

---
+++ Python/easy_BFS.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+
+            # Add neighbors to queue
+            for neighbor in graph[node]:
+                if neighbor not in visited:
+                    queue.append(neighbor)
+
+graph = {
+    "A": ["B", "C"],
+    "B": ["D", "E"],
+    "C": ["F"],
+    "D": [],
+    "E": ["F"],
+    "F": []
+}
+bfs(graph, 'A')
