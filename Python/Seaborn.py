a90a496c03776648b86d6719158a432d618c265d
Devansh <devanshvashisth5@gmail.com>
2025-11-09 12:12:15 +0530

    commit

---
+++ Python/Seaborn.py
@@ -0,0 +1,11 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+import numpy as np
+
+x = np.array([0, 2, 3, 4, 5, 6, 7, 8, 9 ,10, 60])
+y = np.sin(x)
+
+sns.lineplot(x=x, y=y)
+plt.title('Beautiful Line Plot')
+plt.show()
